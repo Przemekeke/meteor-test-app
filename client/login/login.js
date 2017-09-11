3e27e5cfeed5e4b7ec0c43e6aa44ba2b9ec2354e
Przemysław Marciniak <przemyslaw.marciniak2@gmail.com>
2017-09-11 11:03:15 +0200

    View single offer layouts added. Redirecting after login.

--- client/login/login.js
+++ client/login/login.js
@@ -2,3 +2,8 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 
+Template._loginButtonsLoggedInDropdown.events({
+	'click #login-buttons-edit-profile': function(event) {
+		Router.go('profileEdit');
+	}
+});
---
+++ imports/api/offers.js
@@ -0,0 +1,15 @@
+export const Offers = new Mongo.Collection('offers');
+
+ 
+
+if (Meteor.isServer) {
+
+  // This code only runs on the server
+
+  Meteor.publish('offers', function tasksPublication() {
+
+    return Offers.find();
+
+  });
+
+}
